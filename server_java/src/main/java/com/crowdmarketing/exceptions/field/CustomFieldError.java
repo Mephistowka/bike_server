b96f20540c3e654e9981ef01200bc489de50532f
Mephistowka <mephistoshka@gmail.com>
2022-05-30 23:48:13 +0300

    del all usless files

--- server_java/src/main/java/com/crowdmarketing/exceptions/field/CustomFieldError.java
+++
@@ -1,9 +0,0 @@
-package com.crowdmarketing.exceptions.field;
-
-import lombok.Data;
-
-@Data
-public class CustomFieldError {
-    private String field;
-    private String message;
-}
--- server_java/target/classes/db/migration/V3__Add_Roles.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO user_role(user_id, role_id) value (1,1);
-INSERT INTO user_role(user_id, role_id) value (2,2);
